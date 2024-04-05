33f568058e6644358e998402007fedf4bd443559
okuraayno <ohsushi555@gmail.com>
2024-04-05 14:42:37 +0000

    作業

--- README.md
+++ README.md
@@ -23,4 +23,4 @@ Things you may want to cover:
 
 * ...
 
-作業しましたよ
+作業しました
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "ac410d8c895c4744b5df4198fd27e4a3.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
